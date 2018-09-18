b528bc16b60db2eb99d53fc3973042f7309d3f64
Michael Frank <mikepfrank@users.noreply.github.com>
2018-09-18 08:33:07 -0600

    Fix whitespace
    Convert tabs to spaces in loader.lisp

--- src/loader.lisp
+++ src/loader.lisp
@@ -8,22 +8,22 @@
 ;; Load up the system.
 (load "util.lisp")            ; General-purpose utilities.
 (load "infrastructure.lisp")  ; Mechanism for defining and compiling constructs.
-(load "location.lisp")	      ; Describing locations where variables are stored.
+(load "location.lisp")        ; Describing locations where variables are stored.
 (load "environment.lisp")     ; Mapping variables to their locations.
 (load "regstack.lisp")        ; Direct manipulation of registers and the stack.
 (load "variables.lisp")       ; Creating, destroying, moving variables.
-(load "branches.lisp")	      ; Branches, branch pairs, labels.
+(load "branches.lisp")        ; Branches, branch pairs, labels.
 (load "expression.lisp")      ; Binding variables to multiply-nested expressions.
 (load "clike.lisp")           ; C-like assignment-operator statements.
-(load "print.lisp")	          ; Data output.
+(load "print.lisp")           ; Data output.
 (load "controlflow.lisp")     ; High-level conditionals & loops.
 (load "subroutines.lisp")     ; Support for subroutine calls.
 (load "staticdata.lisp")      ; Static data definitions.
-(load "program.lisp")	      ; Highest-level constructs.  Standard library.
+(load "program.lisp")         ; Highest-level constructs.  Standard library.
 (load "library.lisp")         ; Standard library of R routines.
-(load "files.lisp")	          ; File compiler.
+(load "files.lisp")           ; File compiler.
 
 (load "test.lisp")            ; Example programs.
 
 ;; Command to reload the system.
-(defun l () (load "loader.lisp"))			
+(defun l () (load "loader.lisp"))           
